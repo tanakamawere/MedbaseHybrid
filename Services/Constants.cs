e795006eea89d467557e0de458f4b47a3177c2db
Tanaka Mawere <tanakamawere15@gmail.com>
2023-03-02 16:13:29 +0200

    Removed medbase premium

--- Services/Constants.cs
+++ Services/Constants.cs
@@ -6,7 +6,7 @@
         public static double MonthlyAmount() => 3500;
         //Preference strings
         public static string SubscriptionPreferenceDate() => "subscriptionEndDate";
-        public static string apiUrl = "https://medbaseapi.azurewebsites.net/";
+        public static string apiUrl = "https://apimedbase.azurewebsites.net/";
         //public static string apiUrl = "http://localhost:5249/";
         //public static string apiUrl = "http://10.0.2.2:5249/";
     }
