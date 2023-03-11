2ba36f69fe3cbee23a941778f9b93bfa3a6dce0e
Tanaka Mawere <tanakamawere15@gmail.com>
2023-03-11 11:39:37 +0200

    updated to bootstrap 5.3, unable to integrate dark mode

--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -1,25 +1,9 @@
 ﻿using Android.App;
-using Android.Content;
 using Android.Content.PM;
-using Android.OS;
-using MedbaseHybrid.Auth;
-using Microsoft.Identity.Client;
 
 namespace MedbaseHybrid;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
-    protected override void OnCreate(Bundle savedInstanceState)
-    {
-        base.OnCreate(savedInstanceState);
-        // configure platform specific params
-        PlatformConfig.Instance.ParentWindow = this;
-    }
-
-    protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
-    {
-        base.OnActivityResult(requestCode, resultCode, data);
-        AuthenticationContinuationHelper.SetAuthenticationContinuationEventArgs(requestCode, resultCode, data);
-    }
 }
